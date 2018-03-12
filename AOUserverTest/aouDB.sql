fa4d4b738d4c7cc3878ba89ba167d8740bf220d6
Josh <>
2018-03-12 14:29:01 -0400

    deleted 2nd db, merged them

--- AOUserverTest/aouDB.sql
+++ AOUserverTest/aouDB.sql
@@ -35,6 +35,50 @@ CREATE TABLE allofusdbmysql2.userIPAddress (
 
   PRIMARY KEY (Userid)
   );
+
+CREATE TABLE allofusdbmysql2.profileTable (
+  Userid INT NOT NULL AUTO_INCREMENT,
+  Profilepic VARCHAR(45) NULL,
+  Cover1 VARCHAR(255) NULL,
+  Cover2 VARCHAR(255) NULL,
+  Cover3 VARCHAR(255) NULL,
+  Cover4 VARCHAR(255) NULL,
+  Cover5 VARCHAR(255) NULL,
+  Cover6 VARCHAR(255) NULL,
+  Username VARCHAR(45) NULL,
+  Password VARCHAR(45) NULL,
+  Privacy VARCHAR(45) NULL,
+  TimeAccountCreated INT NULL,
+  PRIMARY KEY (Userid)
+  );
+
+CREATE TABLE allofusdbmysql2.userPost (
+  PostID INT NOT NULL AUTO_INCREMENT,
+  Userid INT NOT NULL,
+  Author VARCHAR(255) NULL,
+  Recipient VARCHAR(255) NULL,
+  Photo VARCHAR(255) NULL,
+  Video VARCHAR(255) NULL,
+  Meme1 VARCHAR(255) NULL,
+  Meme2 VARCHAR(255) NULL,
+  TimeAccountCreated INT NULL,
+  PRIMARY KEY (PostID)
+  );
+
+CREATE TABLE allofusdbmysql2.statPost (
+  PostID INT NOT NULL,
+  StatAvg INT NULL,
+  TimeCreated INT NULL,
+  PRIMARY KEY (PostID)
+  );
+
+CREATE TABLE allofusdbmysql2.stats (
+  PostID INT NOT NULL,
+  UseridStat VARCHAR(255) NULL,
+  StatValue VARCHAR(255) NULL,
+  TimeCreated INT NULL,
+  PRIMARY KEY (PostID)
+  );
   
   
 INSERT INTO allofusdbmysql2.UserTable (Userid,fName,lName,Username,Password,MaritalStatus,DateOfBirth,Email,Privacy,TimeAccountCreated)
