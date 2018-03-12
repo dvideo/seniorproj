b534e35e45309d598a3ffee9eeb29f6178c64b29
Josh <>
2018-03-12 14:24:18 -0400

    deleted folder

--- seniorproj-master/AOUserverTest/javascript/meme.js
+++
@@ -1,9 +0,0 @@
-window.onload = function()
-{
-	document.getElementById('memeid').addEventListener("click", memecontrolax);
-}
-function memecontrolax(){
-	document.getElementById("block1").className = "memeytext";
-	document.getElementById("block2").className = "memeytext";
-}
-
