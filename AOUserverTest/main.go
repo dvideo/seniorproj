81099a2a6bce7b9840fd5d88cc9031be1bcf801c
Michael Nelson <michaelnelson@Michaels-MacBook-Pro-3.local>
2018-03-26 22:45:05 -0400

    deleted unneccesary line

--- AOUserverTest/main.go
+++ AOUserverTest/main.go
@@ -395,7 +395,6 @@ func seesionHandling(){
 
 func main() {
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-    http.Handle("/css/", http.FileServer(http.Dir("css/")))    
     db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/allofusdbmysql2") //3306 - johnny //8889 - josh //8889 - elijah
     if err != nil {
         panic(err.Error())
