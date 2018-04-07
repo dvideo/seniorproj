cd0c97b265fe8ae53453759bb7a1d14107f8bde6
Elijah Ziobron <elijahziobron@gmail.com>
2018-04-06 20:42:27 -0400

    Update postFunc.js

--- AOUserverTest/static/postFunc.js
+++ AOUserverTest/static/postFunc.js
@@ -1,19 +1,27 @@
 /* Button toggles the posting content */
-					function myFunction() {
-							document.getElementById("myDropdown").classList.toggle("show");
-					}
+	function myFunction() {
+			document.getElementById("myDropdown").classList.toggle("show");
+	}
 
-					// Close the dropdown if the user clicks outside of it
-					window.onclick = function(event) {
-						if (!event.target.matches('..postDropDown')) {
+	// Close the dropdown if the user clicks outside of it
+	window.onclick = function(event) {
+		if (!event.target.matches('..postDropDown')) {
 
-							var dropdowns = document.getElementsByClassName("dropdown-content");
-							var i;
-							for (i = 0; i < dropdowns.length; i++) {
-								var openDropdown = dropdowns[i];
-								if (openDropdown.classList.contains('show')) {
-									openDropdown.classList.remove('show');
-								}
-							}
-						}
-					}
+			var dropdowns = document.getElementsByClassName("dropdown-content");
+			var i;
+			for (i = 0; i < dropdowns.length; i++) {
+				var openDropdown = dropdowns[i];
+				if (openDropdown.classList.contains('show')) {
+					openDropdown.classList.remove('show');
+				}
+			}
+		}
+	}
+	
+					
+					
+	function commentPost() {
+			document.getElementById("commentB").classList.toggle("show");
+	}
+
+				
